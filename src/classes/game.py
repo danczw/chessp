93eb2770bb4d1abb6345e13059170c219e7c3322
danczw <dev.dc@pm.me>
2022-12-10 18:15:28 +0100

    add game interaction basics

--- src/classes/game.py
+++ src/classes/game.py
@@ -1,6 +1,23 @@
+from classes.board import Board
+
+
 class Game():
     """Game state class
     """
-    def __init__(self):
+    def __init__(self, board: Board):
+        """Initialize a game
+
+        Args:
+            board (Board): board status
+        """
         self.n_turns = 0
         self.turn_white = True
+        self.board = board
+        
+        board.draw()
+    
+    def toggle_turn(self) -> None:
+        """Toggle turn
+        """
+        self.turn_white = not self.turn_white
+        self.n_turns += 1
--- src/main.py
+++ src/main.py
@@ -1,4 +1,22 @@
-from classes.game import *
+from classes.board import Board
+from classes.game import Game
+
+
+def main():
+    print('Hello Chess!')
+    print('Enter moves in the format: e2:e4, a6:b5, f5:f1, etc.')
+    print('<StartcolStartrow>:<EndcolEndrow>')
+    print('Enter "exit" to quit')
+    
+    board = Board()
+    game = Game(board=board)
+    
+    while True:
+        move = input('Enter move: ').lower()
+        if move == 'exit':
+            print('Goodbye Chess!')
+            break
+
 
 if __name__ == '__main__':
-    game = Game()
+    main()
